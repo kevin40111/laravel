c8134040f5dc9bb34cb1f5ffc6d93743304b5aa2
Jung-Yi, Tsai <jungyitsai@gmail.com>
2023-07-11 20:43:32 +0800

    fix: remove confirmed field for frontend

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'username' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|confirmed|min:6',
+            'password' => 'required|string|min:6',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
