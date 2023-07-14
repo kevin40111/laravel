0d367b5ae79eca09b1ba4bcc69e404ed7e167810
ender503 <ender503.tw@gmail.com>
2023-07-14 18:19:24 +0800

    feat: revise auth API

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -14,28 +14,21 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public $timestamps = true;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'fullName',
-        'username',
-        'role',
-        'email',
-        'password',
-    ];
+    protected $fillable = ["fullName", "username", "role", "email", "password"];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ["password", "remember_token"];
 
     /**
      * The attributes that should be cast.
@@ -43,14 +36,16 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        "email_verified_at" => "datetime",
     ];
 
-    public function getJWTIdentifier() {
+    public function getJWTIdentifier()
+    {
         return $this->getKey();
     }
-    
-    public function getJWTCustomClaims() {
+
+    public function getJWTCustomClaims()
+    {
         return [];
-    }    
+    }
 }
